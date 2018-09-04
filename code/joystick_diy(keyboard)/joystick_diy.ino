f1a21b867203a3f52c483efc45bdbf6bffd2f6b3
Taelim Hwang <ghkdxofla@gmail.com>
2018-09-04 20:27:40 +0900

    upload arduino code

---
+++ code/joystick_diy(keyboard)/joystick_diy.ino
@@ -0,0 +1,18 @@
+const int button_start = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(button_start, INPUT_PULLUP);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(button_start) == LOW)
+    Serial.println("button!");
+  delay(10);
+
+}
+
